4781159ae37726c9236657f99d47529793dde22c
DecimoWindow <marcosefrainz@gmail.com>
2026-02-16 23:19:39 -0300

    se implementa fase 2 del proyecto

--- src/logic.py
+++ src/logic.py
@@ -3,10 +3,19 @@ import pandas as pd
 import csv
 from scraper import extraer_precio
 
-mis_activos = pd.read_csv("/Users/marcosefrainzunigahernandez/Desktop/InverTrack-Chile/data/mis_activos.csv")
 
-columnas = mis_activos.apply("ticket")
-abierto = "Close"
+mis_activos = pd.read_csv("C:/Users\Marcos/Desktop/InverTrack-Chile/data/mis_activos.csv") #* Ruta para abrir en win 
+#mis_activos = pd.read_csv("/Users/marcosefrainzunigahernandez/Desktop/InverTrack-Chile/data/mis_activos.csv") #!ruta para abrir en mac
 
-extraer_precio(columnas,abierto)
-print(extraer_precio)
+
+
+mis_activos["precio_actual"] = mis_activos["ticket"].apply(extraer_precio, args=("Open",))
+print("-" * 50)
+
+
+mis_activos["valor_total"] = mis_activos["cantidad"] * mis_activos["precio_actual"]
+
+
+patrimonio_total = mis_activos["valor_total"].sum()
+
+print(f"{patrimonio_total:,.2f}")
