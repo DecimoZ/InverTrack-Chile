05c8019f7d8e43262e563b4073ce5ee1a77f4b61
DecimoWindow <marcosefrainz@gmail.com>
2026-02-22 19:31:43 -0300

    se termina el proyecto implementando la ultima funcion para agregar la fecha como nombre de los archivos

--- src/logic.py
+++ src/logic.py
@@ -2,14 +2,18 @@ import yfinance as yf
 import pandas as pd 
 import csv
 from scraper import extraer_precio
+from datetime import datetime
 
 
 mis_activos = pd.read_csv("C:/Users\Marcos/Desktop/InverTrack-Chile/data/mis_activos.csv") #* Ruta para abrir en win 
 #mis_activos = pd.read_csv("/Users/marcosefrainzunigahernandez/Desktop/InverTrack-Chile/data/mis_activos.csv") #!ruta para abrir en mac
 
 def crear_csv(archivo):
-    nombre = input("Ingrese el nombre para el archivo:")
-    nuevo_archivo = archivo.to_csv(f"C:/Users\Marcos/Desktop/InverTrack-Chile/data/{nombre +".csv"}",index=False)
+    fecha_actual = datetime.now().strftime("%Y-%m-%d")
+    extension = ".csv"
+    nombre_archivo = f"C:/Users\Marcos/Desktop/InverTrack-Chile/data/{fecha_actual + extension}"
+    nuevo_archivo = archivo.to_csv(nombre_archivo,index=False,encoding = "utf-8-sig")
+    print(f"Su archivo fue guardado con el nombre de :{fecha_actual} y fue guardado en con la carpeta {nombre_archivo}")
     return nuevo_archivo
 
 mis_activos["precio_actual"] = mis_activos["ticket"].apply(extraer_precio, args=("Open",))
