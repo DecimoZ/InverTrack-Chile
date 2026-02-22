71d44e6b35d51ff6e79213ea23cf78eb3c6da788
DecimoWindow <marcosefrainz@gmail.com>
2026-02-22 19:12:12 -0300

    se implemeta funcion para crear los archivos en la carpeta data

--- src/logic.py
+++ src/logic.py
@@ -7,15 +7,19 @@ from scraper import extraer_precio
 mis_activos = pd.read_csv("C:/Users\Marcos/Desktop/InverTrack-Chile/data/mis_activos.csv") #* Ruta para abrir en win 
 #mis_activos = pd.read_csv("/Users/marcosefrainzunigahernandez/Desktop/InverTrack-Chile/data/mis_activos.csv") #!ruta para abrir en mac
 
-
+def crear_csv(archivo):
+    nombre = input("Ingrese el nombre para el archivo:")
+    nuevo_archivo = archivo.to_csv(f"C:/Users\Marcos/Desktop/InverTrack-Chile/data/{nombre +".csv"}",index=False)
+    return nuevo_archivo
 
 mis_activos["precio_actual"] = mis_activos["ticket"].apply(extraer_precio, args=("Open",))
 print("-" * 50)
 
-
 mis_activos["valor_total"] = mis_activos["cantidad"] * mis_activos["precio_actual"]
-
-
 patrimonio_total = mis_activos["valor_total"].sum()
 
-print(f"{patrimonio_total:,.2f}")
+
+
+crear_csv(mis_activos)
+
+ 
